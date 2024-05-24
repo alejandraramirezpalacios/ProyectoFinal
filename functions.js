a9520e57afa42ce9ab96074dcdc56605f7bc3164
unknown <alejandraramirezpalacios3@gmail.com>
2024-05-23 22:46:06 -0500

    pagina terminada

---
+++ functions.js
@@ -0,0 +1,15 @@
+const formularioId = document.getElementById('formularioId');
+
+const nombre = document.getElementById('nombre');
+
+const email = document.getElementById('email');
+
+const telefono = document.getElementById('telefono');
+
+const mensaje = document.getElementById('mensaje');
+
+const enviar = document.getElementById('enviar');
+
+formularioId.addEventListener('submit', function(){
+    alert('Su furmulario se ha envíado');
+})
